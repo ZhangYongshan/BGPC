78b984acdb4b5d6ddc09757b34c6f5fd347224fd
Yongshan <54622268+ZhangYongshan@users.noreply.github.com>
2024-05-15 22:06:42 +0800

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,24 @@
+clear; close all; clc;
+addpath(genpath('funs'));
+addpath('Data');
+
+%%
+dataType = 'Salinas';   
+projDim = 84;   % 51 - 102
+
+% dataType = 'PaviaU';
+% projDim = 28;    % 25 - 52
+
+% dataType = 'PaviaC';
+% projDim = 38;     % 25 - 51
+
+%%
+
+[data3D, gt, ind, c] = loadHSI(dataType);
+
+[y_pred, Z, A, W] = BGPC(data3D, c, projDim);
+
+clusteringResults = evaluate_results_clustering(y_pred(ind), gt(ind));
+
+fprintf('\n\n');
+disp(clusteringResults);
